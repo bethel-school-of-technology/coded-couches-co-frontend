360aefc0d56dea18827e37066816f5651a16bbae
Steven <sgalland@mail.com>
2021-08-30 16:56:20 -0700

    added sign up component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AboutMe from './Components/AboutMe';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
     
     <AboutMe />
+    <SignUp />
 
     </div>
   );
---
+++ src/Components/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignUp = () => {
+return (
+    <div>
+        Sign in Here
+    </div>
+)
+
+}
+
+export default SignUp;
