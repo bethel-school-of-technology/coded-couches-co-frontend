434f066e5ff462ae1b7ddaa7e8aef054f8514d74
Steven <sgalland@mail.com>
2021-08-30 17:28:00 -0700

    added name input field, password input field, non functional submit button

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const SignUp = () => {
 return (
-    <div>
-        Sign in Here
-    </div>
+    <form>
+        <p>Add Name</p>
+        <input type="text" name="name" placeholder="name"></input>
+        <p>Create Password</p>
+        <input type="password" name="password" placeholder="password"></input>
+        <br></br>
+        <button type="submit">Submit</button>
+    </form>
 )
 
 }
