94ac62f5625560669f33569fbe43d5612de565f0
Nichole ONeill <nazm612@gmail.com>
2021-09-03 12:01:11 -0700

    added 6 images to the shop page

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,11 +1,24 @@
 import React from 'react';
+//import img1 from 'Images';
 
 const Shop = () => {
+    
     return (
-        <h1>
-            This is the Shop page!
-        </h1>
+        <div>
+            <h1>
+                This is the Shop page!
+            </h1>
+            <img src={'./Components/Images/img1.jpg'} alt='img1' />
+            <img src={'./Components/Images/img2.jpg'} alt='img2' />
+            <img src={'./Components/Images/img3.jpg'} alt='img3' />
+            <img src={'./Components/Images/img4.jpg'} alt='img4' />
+            <img src={'./Components/Images/img5.jpg'} alt='img5' />
+            <img src={'./Components/Images/img6.jpg'} alt='img6' />
+            
+            
+        </div>
     );
+    
 };
 
 export default Shop;
