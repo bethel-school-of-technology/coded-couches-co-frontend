4f9043e69667bd3f6215059c711012b5acc222fb
Nichole ONeill <nazm612@gmail.com>
2021-09-03 12:43:52 -0700

    added a Shop.css file to add styling

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Shop.css';
+
 
 const imgStyle = {
-    height: '150px',
-    width: '150px',
-    padding: '10px'
+    
 
 };
 
@@ -11,15 +11,13 @@ const Shop = () => {
     
     return (
         <div>
-            <h1>
-                This is the Shop page!
-            </h1>
-            <img style={imgStyle} src={'./Components/Images/img1.jpg'} alt='img1' /><br/>
-            <img style={imgStyle} src={'./Components/Images/img2.jpg'} alt='img2' /><br/>
-            <img style={imgStyle} src={'./Components/Images/img3.jpg'} alt='img3' /><br/>
-            <img style={imgStyle} src={'./Components/Images/img4.jpg'} alt='img4' /><br/>
-            <img style={imgStyle} src={'./Components/Images/img5.jpg'} alt='img5' /><br/>
-            <img style={imgStyle} src={'./Components/Images/img6.jpg'} alt='img6' /><br/>
+            
+            <img style={imgStyle} src={'src/Components/Images/img1.jpg'} alt='img1' /><br/>
+            <img style={imgStyle} src={''} alt='img2' /><br/>
+            <img style={imgStyle} src={''} alt='img3' /><br/>
+            <img style={imgStyle} src={''} alt='img4' /><br/>
+            <img style={imgStyle} src={''} alt='img5' /><br/>
+            <img style={imgStyle} src={''} alt='img6' /><br/>
             
             
         </div>
