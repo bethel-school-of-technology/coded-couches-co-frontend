1c892023ea7aa463ab08fffb279651bca46240f9
Nichole ONeill <nazm612@gmail.com>
2021-09-03 12:06:01 -0700

    added className='shopImage' to img tags

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -8,12 +8,12 @@ const Shop = () => {
             <h1>
                 This is the Shop page!
             </h1>
-            <img src={'./Components/Images/img1.jpg'} alt='img1' />
-            <img src={'./Components/Images/img2.jpg'} alt='img2' />
-            <img src={'./Components/Images/img3.jpg'} alt='img3' />
-            <img src={'./Components/Images/img4.jpg'} alt='img4' />
-            <img src={'./Components/Images/img5.jpg'} alt='img5' />
-            <img src={'./Components/Images/img6.jpg'} alt='img6' />
+            <img className="shopImage" src={'./Components/Images/img1.jpg'} alt='img1' />
+            <img className="shopImage" src={'./Components/Images/img2.jpg'} alt='img2' />
+            <img className="shopImage" src={'./Components/Images/img3.jpg'} alt='img3' />
+            <img className="shopImage" src={'./Components/Images/img4.jpg'} alt='img4' />
+            <img className="shopImage" src={'./Components/Images/img5.jpg'} alt='img5' />
+            <img className="shopImage" src={'./Components/Images/img6.jpg'} alt='img6' />
             
             
         </div>
