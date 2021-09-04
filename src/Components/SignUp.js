101c2d209c4f34630835cffee0395e851cc58adb
Steven <sgalland@mail.com>
2021-09-04 14:04:59 -0700

    added code for user post request, still needs linked to backend and DB

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,32 +1,35 @@
 import axios from "axios";
-import React, { useState } from "react"; 
+import React, { useEffect, useState } from "react"; 
+
+///////Is not functional right now, needs to be connected to backend and db see axios post url
+
 
 const SignUp = () => {
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     
     const signIn = (e) => {
         e.preventDefault();
         
-        if (name !== "" && description !== "") {
+        if (username !== "" && password !== "") {
             const req = {
-                name: name,
-                description: description
+                username: username,
+                password: password
             };
                 //add "post user url" in place or url
-            axios.post("http://localhost:3000/inventories", req).then(result => {
+            axios.post("url", req).then(result => {
                 console.log(result.data);
             })
         }
     };
     return (<div>
         <form onSubmit={ signIn }>
-            <h1>Inventory</h1>
-            <label>Item Name</label>
-            <input type="text" name="name" onChange={ e => setName(e.target.value)}></input> <br></br>
-            <label>Description</label> 
-            <input type="text" name="description" onChange={ e => setDescription(e.target.value)}></input> <br></br>
-            <button>Add Item</button>
+            <h1>Sign Up!</h1>
+            <label>Username</label>
+            <input type="text" name="username" onChange={ e => setUsername(e.target.value)}></input> <br></br>
+            <label>Password</label> 
+            <input type="text" name="password" onChange={ e => setPassword(e.target.value)}></input> <br></br>
+            <button>Sign In</button>
         </form>
     </div>);
 
