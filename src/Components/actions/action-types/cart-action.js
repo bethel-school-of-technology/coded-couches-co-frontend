80f527bcebc207866e1d8d9b63829ecad53551b6
Nichole ONeill <nazm612@gmail.com>
2021-09-06 12:59:13 -0700

    deleted action and reducers folders and files.

--- src/Components/actions/action-types/cart-action.js
+++
@@ -1,6 +0,0 @@
-//Types should be in const to avoid typos and duplication since it's a string and could be easily miss spelled
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-export const SUB_QUANTITY = 'SUB_QUANTITY';
-export const ADD_QUANTITY = 'ADD_QUANTITY';
-
--- src/Components/actions/cartActions.js
+++
@@ -1,9 +0,0 @@
-import { ADD_TO_CART } from './action-types/cart-action';
-
-//add cart action
-export const addToCart = (id) => {
-    return {
-        type: ADD_TO_CART,
-        id
-    }
-}
