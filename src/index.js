470bedb33c4505ed4fb308406d2e0360f0f85325
Nichole ONeill <nazm612@gmail.com>
2021-09-06 12:52:46 -0700

    restored index.js file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import cartReducer from './Components/reducers/cartReducer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import reportWebVitals from './reportWebVitals';
 
-const store = createStore(cartReducer);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
