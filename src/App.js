f105bce665328ecad07cbd5fe0f535ab4f35cfbd
Nichole ONeill <nazm612@gmail.com>
2021-09-07 09:51:42 -0700

    moved navbar from App.js to its own file Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,10 @@
 import React from 'react';
-import logo from './Images/logo.jpg';
 import './App.css';
-import AboutMe from './Components/AboutMe';
-import SignUp from './Components/SignUp';
-import Home from './Components/Home';
-import Shop from './Components/Shop';
-import Login from './Components/Login';
-import Cart from './Components/Cart';
-import Admin from './Components/Admin';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <Router>
-    <div>
-        <nav>
-        <img className="logo" src={logo} alt="logo" />
-        <div>  
-         <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-            <li>
-              <Link to="/admin">Admin</Link>
-            </li>
-          </ul>
-        </div>
-        </nav>
-
-
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about">
-            <AboutMe />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/admin">
-            <Admin />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Navbar />
   );
 }
 export default App;
