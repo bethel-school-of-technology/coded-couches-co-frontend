eb5ed160c6fd09be3b4bb2d571ab3231b41bbf74
Steven <sgalland@mail.com>
2021-09-07 17:51:33 -0700

    got the delete inventory function working correctly React side

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 
 const Admin = ({history}) => {
+    
+    
     //get inventory 
     const [inventories, setInv] = useState([]);
     useEffect(() => {   
@@ -11,7 +13,8 @@ const Admin = ({history}) => {
         })
     }, []);
 
-        //get users
+        
+    //get users
     const [users, setUsers] = useState([]);
     useEffect(() => {
         axios.get("http://localhost:3000/users").then(result => {
@@ -19,18 +22,14 @@ const Admin = ({history}) => {
         })
     }, []);
 
-    // test button for users
-// function test() {
-//     console.log(users);
-// }
 
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     // const [id, setId] = useState("");
 
+    
+    // create inventory
     const create = () => {
-
-        
         if (name !== "" && description !== "") {
             const req = {
                 name: name,
@@ -53,15 +52,15 @@ const Admin = ({history}) => {
         }
     };
 
-
-    // can manually delete with this function, but cant automate it.
-    const Delete = (e) => {
-        const url = `http://localhost:3000/inventories/inv/${inventories.id}`;
-        e.preventDefault();
-        //update url to match zachs coding, this is a temporary url.
+    
+    // delete inventory :from axios website axios.delete(url[, config])
+    // can manually delete with this function, but cant automate it. update url to match zachs coding, this is a temporary url.
+    const Delete = (id) => {
+        const url = ("http://localhost:3000/inventories/inv/" + id);
         axios.delete(url)
     .then(res => res.data)
     }
+
 
 
 
@@ -75,19 +74,21 @@ const Admin = ({history}) => {
             <input type="text" name="description" onChange={ e => setDescription(e.target.value)}></input> <br></br>
             <button>Add Item</button>
         </form>
+
+
+        
     <h1>All the Inventory</h1>
     <ul>
     <form>
     { inventories.map(inventories => 
             <li key={inventories.id}>
                 INVENTORY ID:{inventories.id} <br /> NAME: {inventories.name} <br /> DESCRIPTION: {inventories.description} <br />
-                <button onClick={ Delete }>REMOVE</button>
+                <button onClick={() => Delete(inventories.id) }>REMOVE</button>
             </li>
             
             ) }
     </form>
     </ul> 
-    {/* <button onClick={test}>this is a test</button> */}
 
 
     <h1>All the Users</h1>
@@ -98,7 +99,6 @@ const Admin = ({history}) => {
             </li>
             ) }
     </ul> 
-    {/* <button onClick={test}>this is a test</button> */}
     </div>
 );
 
