703ae105ff96f052f7de6fd87a37379d359f93ab
Nichole ONeill <nazm612@gmail.com>
2021-09-13 15:14:35 -0700

    added cart summary on the Cart.js file

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Cart = (props) => {
-  const { cartItems, onAdd, onRemove, onCheckout } = props;
+  const { cartItems, onAdd, onRemove } = props;
+  const itemPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+  const taxPrice = itemPrice * 0.056;
+  const shippingPrice = itemPrice > 2000 ? 0 : 50;
+  const orderTotal = itemPrice + taxPrice + shippingPrice;
     
     return (
         <div>
@@ -20,21 +24,41 @@ const Cart = (props) => {
                         </div>
                         <div>
                             {item.qty} x ${item.price.toFixed(2)}
-                        </div> 
-                         
+                        </div>                                  
+                    </div>                    
+                ))}
+                {cartItems.length !== 0 && (
+                    <div>
+                        <hr></hr>
+                        <div>
+                            <div>Item Price</div>
+                            <div>${itemPrice.toFixed(2)}</div>
+                        </div>
+                        <div>
+                            <div>Tax Price</div>
+                            <div>${taxPrice.toFixed(2)}</div>
+                        </div>
+                        <div>
+                            <div>Shipping</div>
+                            <div>${shippingPrice.toFixed(2)}</div>
+                        </div>
                         <div>
                             <div>
-                                <button onClick={() => onCheckout(item)}>Checkout</button>
+                                <strong>Order Total</strong>
+                            </div>
+                            <div>
+                                <strong>${orderTotal.toFixed(2)}</strong>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <button>Checkout</button>
                             </div>    
-                        </div>                 
+                        </div>     
                     </div>
-                    
-                ))}
-                
+                )}
             </div>
-            
         </div>
-
     );
 };
 
--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
-import Cart from './Cart';
 
 const Checkout = () => {
-    const [ checkout, setCheckout ] = useState([]);
-    const itemsInCart = checkout.find((x) => x.qty > 0);
-    
-    const onCheckout = (item) => {
-        if (itemsInCart) {
-            setCheckout (
-                checkout.map((x) =>
-                x.qty > 0 ? {...itemsInCart, qty: itemsInCart.qty } : x
-                )
-            );
-        } else {
-            setCheckout([...itemsInCart, {...item, qty: 1}]);
-        }
-        console.log("Checked Out!");
-    };
-
     return(
         <div>
-            {checkout.map((checkout) => (
-                <Cart key={checkout.id} onCheckout={onCheckout}></Cart>
-            ))}
         </div>
     )
 }
 
 export default Checkout;
+
+// import Cart from './Cart';
+
+
+    // const [ checkout, setCheckout ] = useState([]);
+    // const itemsInCart = checkout.find((x) => x.qty > 0);
+    
+    // const onCheckout = (item) => {
+    //     if (itemsInCart) {
+    //         setCheckout (
+    //             checkout.map((x) =>
+    //             x.qty > 0 ? {...itemsInCart, qty: itemsInCart.qty } : x
+    //             )
+    //         );
+    //     } else {
+    //         setCheckout([...itemsInCart, {...item, qty: 1}]);
+    //     }
+    //     console.log("Checked Out!");
+    // };
