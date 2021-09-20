fdf099f47b5af5cff529f8f57d40dc5b83b2077b
ZBrinz <ZachBrinsley@gmail.com>
2021-09-20 16:32:10 -0700

    Added ImageUrl Fields as IMAGE, on the add/edit inventory fields. Also edited the shop page to display the url placed inside the database

--- src/Components/pages/store/Product.js
+++ src/Components/pages/store/Product.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import { useState } from 'react';
-import Store from '../../../Store';
+import React from "react";
+import { useState } from "react";
+import Store from "../../../Store";
 const Product = (props) => {
-    const { product } = props;
+  const { product } = props;
 
-    
-    console.log(product);
-    const [cartItems, setCartItems] = useState([]);
+  console.log(product);
+  const [cartItems, setCartItems] = useState([]);
   console.log(cartItems);
-    const onAdd = (product) => {
-        const exist = cartItems.find((x) => x.id === product.id);
-        if (exist) {
-        setCartItems(
-            cartItems.map((x) =>
-            x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
-            )
-        );
-        } else {
-        setCartItems([...cartItems, { ...product, qty: 1 }]);
-        }
-    };
-    
-    return (
-        <div>
-            <img className="small" src="https://m.media-amazon.com/images/I/A1Ev61anEuL._AC_UL320_.jpg" alt={product.name}></img>
-            <h3>{product.name}</h3>
-            <div>{product.description}</div>
-            <div>${product.price}</div>
-            <div>
-                <button onClick={() => onAdd(cartItems)}>Add to Cart</button>
-            </div>
-        </div>
-    );
-}
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  return (
+    <div>
+      <img className="small" src={product.image} alt={product.name}></img>
+      <h3>{product.name}</h3>
+      <div>{product.description}</div>
+      <div>${product.price}</div>
+      <div>
+        <button onClick={() => onAdd(cartItems)}>Add to Cart</button>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
-
