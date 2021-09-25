6e933b13cc249c48fdae2f3a872e67763450b9af
ZBrinz <ZachBrinsley@gmail.com>
2021-09-25 15:20:59 -0700

    added another photo to the slideshow, now four in total

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -2,9 +2,8 @@ import React from 'react';
 import CCC1 from '../../Images/CCC1.png';
 import CCC2 from '../../Images/CCC2.png';
 import CCC3 from '../../Images/CCC3.png';
+import CCC4 from '../../Images/CCC4.png';
 import { useEffect } from 'react';
-
-
 
 
 const Home = () => {
@@ -26,29 +25,14 @@ function nextImage() {
 };
 
 },[])
-
-
     return(
         <header className="intro">
             <img src={CCC1} alt="" className="slideshow-img"/>
             <img src={CCC2} alt="" className="slideshow-img"/>
             <img src={CCC3} alt="" className="slideshow-img"/>
+            <img src={CCC4} alt="" className="slideshow-img"/>
         </header>
-
-
-    );
-
-    
+    );   
 };
-
-
-// export default function Home(props) {
-//     return( <header class="intro">
-//     <img src="/src/Images/CCC1.jpg" alt="" class="slideshow-img">
-//     <img src="/src/Images/CCC2.jpg" alt="" class="slideshow-img">
-//     <img src="/src/Images/CCC3.jpg" alt="" class="slideshow-img">
-//   </header>;
-// );
-// };
 
 export default Home;
