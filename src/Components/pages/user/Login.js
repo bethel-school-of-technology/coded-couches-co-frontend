005d6cb563d83ce7ec8099cd9b116a55ef882713
Steven <sgalland@mail.com>
2021-09-27 12:38:41 -0700

    admin redirect to dashboard was commented out, added it back in

--- src/Components/pages/user/Login.js
+++ src/Components/pages/user/Login.js
@@ -20,23 +20,20 @@ const Login = (props) => {
             axios.post("http://localhost:3000/users/login", req).then(result => {
                 const token = result.data.jwt;
                 const user = result.data.user;
-<<<<<<< HEAD
-=======
                 
                 console.log(user)
->>>>>>> e844153d9f12e6ba1bba72124b4746ebd3d867da
                 localStorage.setItem("myJWT", token);
                 localStorage.setItem("user", JSON.stringify(user));
-                // const admin = result.data.user.admin;
+                const admin = result.data.user.admin;
                 if (token) {
-                    // if (admin) {
-                        // props.history.push({
-                        //     pathname: "/dash",
-                        // });
-                    // } else {
+                    if (admin) {
+                        props.history.push({
+                            pathname: "/dash",
+                        });
+                    } else {
                         props.history.push("/shop");
                     };
-                // }
+                }
             })
         } else {
             return alert("Username needs to be at least 3 characters, and Password at least 6 characters")
