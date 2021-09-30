e9d83a3c0b2b2a9b9223e774ade71ae65deeb96a
Nichole O'Neill <nazm612@gmail.com>
2021-09-30 13:04:55 -0700

    put the profile back on the navbar. fixed some errors in the login file and deleted some commented out code on the cart file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,10 +21,12 @@ const Navbar = () => {
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/shop">Shop</Link></li>
-                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/cart">Cart</Link></li>
                         <li><Link to="/signup">SignUp</Link></li>
-                        <li><Link to="/cart">Cart</Link>
-                        </li>
+                        <li><Link to="/login">Login</Link></li>
+                        
+                        
                         <li><Link onClick={Logout} to="/login">Logout</Link></li>
                     </ul>
                 </nav>
