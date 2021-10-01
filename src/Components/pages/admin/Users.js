d454d0d9623d7fa37c8af3d60148f46155e6d907
Steven <sgalland@mail.com>
2021-10-01 11:28:29 -0700

    created a better readability for users createdAt output

--- src/Components/pages/admin/Users.js
+++ src/Components/pages/admin/Users.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react"; 
 import { withRouter } from "react-router-dom";
 import Layout from "./Layout";
+var moment = require('moment');
 
 const URL = process.env.REACT_APP_API_URL
 
@@ -78,7 +79,7 @@ const Users = (props) => {
                         <td className="tdSmall">{user.id}</td>
                         <td className="tdLarge">{user.username}</td>
                         <td className="tdLarge">{(user.admin === true)? 'is admin': 'is not admin'}</td>
-                        <td className="tdLarge">{user.createdAt}</td>
+                        <td className="tdLarge">{moment(user.createdAt).format("MMMM Do YYYY")}</td>
                         <td><button className="btn-delete" onClick={() => {if (window.confirm("Are you sure?")) DeleteUser(user)}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
